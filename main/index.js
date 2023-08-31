1d2cf6d988ae78737a990cdfda01cc237ba85a1d
pang <pang@teamo2.kr>
2023-08-31 16:58:28 +0900

    Test2 수정

--- main/index.js
+++ main/index.js
@@ -4,6 +4,9 @@ function githubTest() {
 
 function githubTest2() {
     console.log('github test start2');
+    console.log('github test start3');
+    console.log('github test start4');
+    console.log('github test start5');
 }
 
 function githubTest3() {
