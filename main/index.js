6e9079a8c9377f832dde52732bcd4fcec53eb6c8
Gerrard Moon <>
2023-08-31 15:32:59 +0900

    FEAT:(ERP-123) 깃헙 플러그인 init
    - 플러그인 설치
    - github 계정 연동

--- main/index.js
+++ main/index.js
@@ -2,16 +2,14 @@ function githubTest() {
     console.log('github test start');
 }
 
-githubTest();
-
 function githubTest2() {
     console.log('github test start2');
 }
-
-githubTest2();
 
 function githubTest3() {
     console.log('github test start3');
 }
 
+githubTest();
+githubTest2();
 githubTest3();
